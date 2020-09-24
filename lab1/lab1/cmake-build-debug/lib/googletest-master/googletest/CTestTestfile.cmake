e5691bd9b04c850ebbdf9af29a7b91e5acca3c18
VitalTerror <vitaly.spirin.1995@gmail.com>
2020-09-24 23:15:24 +0700

    lab1 v5

--- lab1/lab1/cmake-build-debug/lib/googletest-master/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/NSU_Labs/OOOP/lab1/lib/googletest-master/googletest
-# Build directory: D:/NSU_Labs/OOOP/lab1/cmake-build-debug/lib/googletest-master/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
