f291bbd54baf4ed5b340ad816dd9f768330b8629
VitalTerrorX <vitalterrorx@gmail.com>
2020-12-11 16:34:57 +0700

    csv parser - init

---
+++ CSVParser/cmake-build-debug/CMakeFiles/CSVParser.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CSVParser.dir/main.cpp.obj"
+  "CSVParser.exe"
+  "CSVParser.exe.manifest"
+  "CSVParser.lib"
+  "CSVParser.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CSVParser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
